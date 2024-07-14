3bb86092fb2a680326b24d2c6d750b6b9243eeb4
Rodrigo Marques Barbosa <rodrigo.barbosa@arboimoveis.com>
2024-07-13 23:07:14 -0300

    att dashboard

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -49,15 +49,53 @@ return {
       'rcarriga/nvim-notify',
     },
   },
-
-  -- NOTE:  -->startfy <--
+  -- NOTE:  -->dashboard stuff<--
   {
-    'startup-nvim/startup.nvim',
-    requires = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' },
+    'nvimdev/dashboard-nvim',
+    event = 'VimEnter',
     config = function()
-      require('startup').setup { theme = 'startify' }
+      require('dashboard').setup {
+        theme = 'hyper',
+        config = {
+          week_header = {
+            enable = true,
+          },
+          shortcut = {
+            { desc = '󰊳 Update', group = '@property', action = 'Lazy update', key = 'u' },
+            {
+              icon = ' ',
+              icon_hl = '@variable',
+              desc = 'Files',
+              group = 'Label',
+              action = 'Telescope find_files',
+              key = 'f',
+            },
+            -- {
+            --   desc = ' Apps',
+            --   group = 'DiagnosticHint',
+            --   action = 'Telescope app',
+            --   key = 'a',
+            -- },
+            -- {
+            --   desc = ' dotfiles',
+            --   group = 'Number',
+            --   action = 'Telescope dotfiles',
+            --   key = 'd',
+            -- },
+          },
+        },
+      }
     end,
+    dependencies = { { 'nvim-tree/nvim-web-devicons' } },
   },
+  -- NOTE:  -->startfy <--
+  -- {
+  --   'startup-nvim/startup.nvim',
+  --   requires = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' },
+  --   config = function()
+  --     require('startup').setup { theme = 'startify' }
+  --   end,
+  -- },
   -- NOTE:  -->undotree <--
   {
     'jiaoshijie/undotree',
